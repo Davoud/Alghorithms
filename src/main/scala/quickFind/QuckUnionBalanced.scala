277160eaa96302596ec7be466d03d86d90960e5d
DavoodN <DavoodN@systemgroup.net>
2019-03-12 18:10:39 +0330

    imporvements union finds

--- src/main/scala/quickFind/QuckUnionBalanced.scala
+++ src/main/scala/quickFind/QuckUnionBalanced.scala
@@ -3,41 +3,41 @@ package quickFind
 class QuickFindBalanced(N: Int)
 {
     private val id = new Array[Int](N)
-    private val sizes = scala.collection.immutable.Map[Int,Int]()
+    private val size = new Array[Int](N)
 
-    for(i <- 0 to id.length - 1)
+    for(i <- 0 to id.length - 1) 
+    {
         id(i) = i
+        size(i) = 1
+    }
     
     def root(p: Int) : Int = {        
         var i = p
-        while(i != id(i)) i = id(i)        
+        while(i != id(i)) { 
+            id(i) = id(id(i))
+            i = id(i)        
+        }
         i
     }
 
     def union(p: Int, q: Int) = {
-        var rootp = root(p)
-        var rootq = root(q)
-        
-        var sizep = size(p)
-        var sizeq = size(q)
-
-        if(sizep > sizeq)
-        {
-                 
+        var i = root(p)
+        var j = root(q)               
+        if(i != j) {        
+            if(size(i) < size(j)) {
+                id(i) = j
+                size(j) += size(i)
+            }
+            else {
+                id(j) = i
+                size(i) += size(j)
+            }        
         }
-        else
-        {
-
-        }
-
-        id(rootp) = root(q)       
     }
 
 
     def connected(p: Int, q: Int) : Boolean = root(p) == root(q)
 
-    override def toString = id.deep.mkString(",")
-    
-    def size(p: Int): Int = sizes.getOrElse(p, 1)    
+    override def toString = id.deep.mkString(",")       
 
 }
