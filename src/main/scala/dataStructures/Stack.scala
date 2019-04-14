e9ac3f7c9f0bb31161c2b1249377de955ab49716
Davoud Nasiri Mobarakeh <nas.david@yahoo.com>
2019-04-14 20:56:02 +0430

    Added Stack (link list imp)

---
+++ src/main/scala/dataStructures/Stack.scala
@@ -0,0 +1,48 @@
+package dataStructures
+
+trait Stack[T]
+{
+    def push(item: T)
+    def pop(): T
+    def size(): Int
+    def isEmpty: Boolean
+}
+
+
+case class Node[T](value: T, next: Node[T])
+
+class LinkListStack[T] extends Stack[T]
+{
+    private var first: Node[T]
+    private var size: Int = 0
+
+    override def isEmpty = first == null
+
+    override def push(item: T) = {
+        if(isEmpty)
+        {
+            first = Node(item, null)
+        }
+        else
+        {
+            val newFirst = Node(item, first)
+            first = newFirst
+        }
+    }
+
+    override def pop(): T = {
+        if(!isEmpty) {
+            var oldFirst = first
+            if(first.next != null)
+            {
+                first = first.next
+            }
+            var value = oldFirst.value
+            oldFirst = null
+            value
+        }
+
+        null
+    }
+
+}
