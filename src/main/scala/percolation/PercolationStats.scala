dc65441af027a0277b8d8f6844a3780b28d51045
davoud <nas.david@yahoo.com>
2019-05-23 23:53:38 +0430

    Made all test on percolation passed.

---
+++ src/main/scala/percolation/PercolationStats.scala
@@ -0,0 +1,12 @@
+package percolation
+
+class PercolationStats(n: Int, trails: Int) {
+
+
+
+  def mean(): Double = 0
+  def stddev(): Double = 0
+
+
+
+}
--- src/test/scala/percolation/PercolationTest.scala
+++ src/test/scala/percolation/PercolationTest.scala
@@ -1,12 +1,11 @@
 package percolation
 
 import org.scalatest.FunSuite
-import percolation._
 
 
 class PercolationTest extends FunSuite
 {
-    ignore("All nodes are closed at initiation") {
+    test("All nodes are closed at initiation") {
         val n = 5
         val p = new Percolation(n)
         for(row <- 0 until n)
@@ -14,14 +13,14 @@ class PercolationTest extends FunSuite
                 assert(p.isOpen(row, col) === false)
     }
 
-    ignore(testName = "Opening Nodes") {
+    test(testName = "Opening Nodes") {
         val n = 5
         val p = new Percolation(n)
         p.open(0, 0)
         assert(p.isOpen(0, 0) === true)
     }
 
-    ignore("Open All Nodes") {
+    test("Open All Nodes") {
         val n = 100
         val p = new Percolation(n)
 
@@ -34,12 +33,68 @@ class PercolationTest extends FunSuite
                 assert(p.isOpen(row, col) === true)
     }
 
-    ignore("Is Fill") {
-        val n = 5
+    test("Percolates 1x1") {
+        val n = 1
         val p = new Percolation(n)
-        assert(p.isFull(0, 0) === false)
-        p.isOpen(0, 0)
-        assert(p.isFull(0, 0) === true)
+        assert(p.percolates() === false)
+        p.open(0,0)
+        assert(p.percolates() === true)
     }
 
+    test("Percolates 2x2 route 10/10") {
+        val n = 2
+        val p = new Percolation(n)
+        assert(p.percolates() === false)
+        p.open(0, 0)
+        assert(p.percolates() === false)
+        p.open(1, 0)
+        assert(p.percolates() === true)
+    }
+
+    test("Not percolates 2x2 route 10/01") {
+        val n = 2
+        val p = new Percolation(n)
+        p.open(0, 0)
+        assert(p.percolates() === false)
+        p.open(1,1)
+        assert(p.percolates() === false)
+    }
+
+    test("Percolates 10x10 route: middle vertial line"){
+        val n = 10
+        val p = new Percolation(n)
+
+        assert(p.percolates() === false)
+
+        for(row <- 0 until n)
+            p.open(row, 5)
+
+        assert(p.percolates() === true)
+    }
+
+    test("Not percolates 10x10 diagonal") {
+        val n = 10
+        val p = new Percolation(n)
+
+        assert(p.percolates() === false)
+
+        for(i <- 0 until n)
+            p.open(i, i)
+
+        assert(p.percolates() === false)
+    }
+
+    test("Number of open sites 10x10 diagonal") {
+        val n = 10
+        val p = new Percolation(n)
+
+        assert(p.numberOfOpenSites() === 0)
+
+        for(i <- 0 until n) {
+            p.open(i, i)
+            assert(p.numberOfOpenSites() === i+1)
+        }
+
+        assert(p.numberOfOpenSites() === 10)
+    }
 }
