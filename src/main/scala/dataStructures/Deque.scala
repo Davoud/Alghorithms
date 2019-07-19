6e363109d040be1d50798a74f06a31e90213e6e5
Davoud <nas.david@yahoo.com>
2019-07-19 13:16:11 +0430

    Added RandomizedQueue and Deque

--- src/main/scala/dataStructures/Deque.scala
+++ src/main/scala/dataStructures/Deque.scala
@@ -2,21 +2,73 @@ package dataStructures
 
 class Deque[A] extends Iterable[A] {
 
-  def isDequeEmpty: Boolean = true
+  private var _first: Option[Node[A]] = None
+  private var _last: Option[Node[A]] = None
+  private var _size: Int = 0
 
-  def dequSize: Int = 0
+  def isDequeEmpty: Boolean = _size == 0
 
-  def addFirst(item: A): Unit = ???
+  def dequeSize: Int = _size
 
-  def addLast(item: A): Unit = ???
+  def addFirst(item: A): Unit = {
+    _first = Some(new Node[A](item, _first, None))
+    _size += 1
+    if (_size == 1)
+      _last = _first
+  }
 
-  def removeFirst(item: A): A = ???
+  def addLast(item: A): Unit = {
+    val newLast = new Node[A](item, None, _last)
+    _last.get.next = Some(newLast)
+    _last = Some(newLast)
+    _size += 1
+    if (_size == 1)
+      _first = _last
+  }
 
-  def removeLast(item: A): A = ???
+  def removeFirst(): A = {
+    if (_first.isDefined) {
+      _size -= 1
+      val firstNode = _first.get
+      _first = firstNode.next
+      return firstNode.value
+    }
+    throw new Exception("Deque is empty")
+  }
 
-  def peakFirst(): A = ???
+  def removeLast(): A = {
+    if (_last.isDefined) {
+      _size -= 1
+      val lastNode = _last.get
+      _last = lastNode.prev
 
-  def peakLast(): A = ???
+      if (_last.isDefined)
+        _last.get.next = None
+      else
+        _first = None
 
-  override def iterator: Iterator[A] = ???
+      return lastNode.value
+    }
+    throw new Exception("Deque is empty")
+  }
+
+  def peakFirst(): Option[A] = if (_first.isDefined) Some(_first.get.value) else None
+
+  def peakLast(): Option[A] = if (_last.isDefined) Some(_last.get.value) else None
+
+
+  override def iterator: Iterator[A] = new DequeIterator[A](_first)
+
+  class DequeIterator[T](private var node: Option[Node[T]]) extends Iterator[T] {
+    override def hasNext: Boolean = node.isDefined
+
+    override def next(): T = {
+      val value = node.get.value
+      node = node.get.next
+      return value
+    }
+  }
+
+
+  private case class Node[A](value: A, var next: Option[Node[A]], var prev: Option[Node[A]])
 }
---
+++ src/main/scala/dataStructures/RandomizedQueue.scala
@@ -0,0 +1,90 @@
+package dataStructures
+
+import edu.princeton.cs.algs4.StdRandom
+
+
+class RandomizedQueue[A: Manifest](capacity: Int = 4) extends Iterable[A] {
+
+  private var queue = new Array[Option[A]](capacity)
+  private var tailIndex: Int = 0
+
+  def isQueueEmpty: Boolean = tailIndex == 0
+
+  def queueSize: Int = tailIndex
+
+  def enqueue(item: A): Unit = {
+    if (tailIndex == queue.length) resize(queue.length * 2)
+    if (tailIndex == queue.length / 4) resize(queue.length / 2)
+
+    if (tailIndex == 0) {
+      queue(tailIndex) = Some(item)
+    }
+    else {
+      queue(tailIndex) = Some(item)
+      exchange(queue, tailIndex, StdRandom.uniform(tailIndex))
+    }
+    tailIndex += 1
+  }
+
+  private def resize(length: Int): Unit = {
+    val newQueue = new Array[Option[A]](length)
+    for (i <- 0 until tailIndex)
+      newQueue(i) = queue(i)
+    queue = newQueue
+  }
+
+  private def exchange(a: Array[Option[A]], i: Int, j: Int): Unit = {
+    val temp = a(i)
+    a(i) = a(j)
+    a(j) = temp
+  }
+
+  def dequeue(): A = {
+    if (tailIndex > 0) {
+      tailIndex -= 1
+      val item = queue(tailIndex).get
+      queue(tailIndex) = None
+      return item
+    }
+    throw new NoSuchElementException("Queue is empty")
+  }
+
+  def sample(): A =
+    if (tailIndex > 0)
+      queue(StdRandom.uniform(tailIndex)).get
+    else
+      throw new NoSuchElementException("Queue is empty")
+
+  override def iterator: Iterator[A] = new QueueIterator[A](queue, tailIndex)
+
+  class QueueIterator[T: Manifest](q: Array[Option[T]], tailIndex: Int) extends Iterator[T] {
+
+    var index: Int = 0
+    val shuffledQueue = shuffle(q)
+
+    private def shuffle(a: Array[Option[T]]): Array[T] = {
+      val q = new Array[T](tailIndex)
+
+      for (i <- 0 until tailIndex)
+        q(i) = a(i).get
+
+      q.indices.foreach(i => exchange(q, i, StdRandom.uniform(i + 1)))
+      return q
+    }
+
+    private def exchange(a: Array[T], i: Int, j: Int): Unit = {
+      val temp = a(i)
+      a(i) = a(j)
+      a(j) = temp
+    }
+
+    override def hasNext: Boolean = index < tailIndex
+
+    override def next(): T = {
+      val item = shuffledQueue(index)
+      index += 1
+      item
+    }
+  }
+
+}
