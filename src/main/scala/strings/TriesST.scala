4a050044f4524386c3d1d9a8373de6395daca646
Davoud <nas.david@yahoo.com>
2020-10-06 09:10:22 +0330

    Completed Tries Symbol Table and its tests

--- src/main/scala/strings/TriesST.scala
+++ src/main/scala/strings/TriesST.scala
@@ -1,13 +1,17 @@
 package strings
 
+import scala.collection.mutable
+
 
 class TriesST[Value: Manifest] {
+	
+	type StringQ = mutable.Queue[String]
 	
 	private val R = 256
 	private var root: Option[Node] = None
 	
 	case class Node(var value: Option[Value] = None) {
-		val next = new Array[Node](R)
+		val next: Array[Option[Node]] = Array.fill(R)(None)
 	}
 	
 	def get(key: String): Option[Value] = get(root, key, 0) match {
@@ -15,26 +19,102 @@ class TriesST[Value: Manifest] {
 		case None => None
 	}
 	
-	def put(key: String, value: Value): Unit = root = Option(put(root, key, value, 0))
+	def put(key: String, value: Value): Unit = root = put(root, key, value, 0)
 	
-	def put(x: Option[Node], key: String, value: Value, d: Int): Node = {
+	def size: Int = size(root)
+	
+	def keys(): Iterable[String] = keysWithPrefix("")
+	
+	def keysWithPrefix(pre: String): Iterable[String] = {
+		val q = new mutable.Queue[String]()
+		collect(get(root, pre, 0), pre, q)
+		q
+	}
+	
+	def keysThatMatch(pattern: String): Iterable[String] = {
+		val q = new mutable.Queue[String]()
+		collect(root, "", pattern, q)
+		q
+	}
+	
+	def longestPrefixOf(s: String): String = s.substring(0, search(root, s, 0, 0))
+	
+	def remove(key: String): Unit = root = delete(root, key, 0)
+	
+	
+	private def delete(x: Option[Node], key: String, d: Int): Option[Node] = {
+		if (x.isEmpty) return None
+		if (d == key.length) {
+			x.get.value = None
+		}
+		else {
+			x.get.next(key(d)) = delete(x.get.next(key(d)), key, d + 1)
+		}
+		
+		if (x.get.value.isDefined) return x
+		
+		for (c <- 0 until R)
+			if (x.get.next(c) != null)
+				return x
+		
+		None
+		
+	}
+	
+	
+	private def search(x: Option[Node], s: String, d: Int, length: Int): Int = {
+		if (x.isEmpty) return length
+		val len = if (x.get.value.isDefined) d else length
+		if (d == s.length) return len
+		search(x.get.next(s(d)), s, d + 1, len)
+	}
+	
+	private def collect(x: Option[Node], pre: String, pattern: String, q: StringQ): Unit = {
+		if (x.isEmpty) return
+		val d = pre.length
+		if (d == pattern.length && x.get.value.isDefined) q enqueue pre
+		if (d == pattern.length) return
+		val next = pattern(d)
+		for (c <- 0 until R)
+			if (next == '.' || next == c)
+				collect(x.get.next(c), pre + c.toChar, pattern, q)
+	}
+	
+	private def collect(x: Option[Node], pre: String, q: StringQ): Unit = {
+		if (x.isEmpty) return
+		if (x.get.value.isDefined) q enqueue pre
+		for (c <- 0 until R)
+			collect(x.get.next(c), pre + c.toChar, q)
+	}
+	
+	private def put(x: Option[Node], key: String, value: Value, d: Int): Option[Node] = {
 		val y = if (x.isDefined) x.get else Node()
 		if (d == key.length) {
 			y.value = Some(value)
-			return y
+			return Some(y)
 		}
 		val c = key(d)
-		y.next(c) = put(Option(y.next(c)), key, value, d + 1)
-		y
+		y.next(c) = put(y.next(c), key, value, d + 1)
+		Some(y)
 	}
 	
 	private def get(x: Option[Node], key: String, d: Int): Option[Node] = x match {
 		case None => None
 		case Some(y) => key.length match {
 			case len if (len == d) => Some(y)
-			case _ => get(Option(y.next(key(d))), key, d + 1)
+			case _ => get(y.next(key(d)), key, d + 1)
 		}
 	}
+	
+	private def size(x: Option[Node]): Int = {
+		if (x.isEmpty) return 0
+		var count = 0
+		if (x.get.value.isDefined) count += 1
+		for (c <- 0 until R)
+			count += size(x.get.next(c))
+		count
+	}
+	
 	
 	@inline def update(key: String, value: Value): Unit = put(key, value)
 	
--- src/test/scala/strings/TriesTest.scala
+++ src/test/scala/strings/TriesTest.scala
@@ -4,10 +4,40 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TriesTest extends FlatSpec with Matchers {
 	
+	val text = "The code that we have considered is a compact and complete implementation " +
+		"of the string symbol-table API that has broadly useful practical applications Several variations " +
+		"and extensions are discussed in the exercises. Next, we consider basic properties of tries and some " +
+		"limitations on their utility The code that we have considered is a compact and complete implementation " +
+		"discussed in the exercises. Next, we consider basic properties of limitations on their utility The code " +
+		"that we have considered is a compact "
+	
+	def sample(): TriesST[Int] = {
+		val t = new TriesST[Int]()
+		
+		for (w <- text.split(' '))
+			t(w) match {
+				case None => t(w) = 1
+				case Some(n) => t(w) = n + 1
+			}
+		
+		t
+	}
+	
 	"TriesST" should "get single item" in {
 		val t = new TriesST[Int]()
 		t("a") = 1
 		t("a") should be(Some(1))
+		
+	}
+	
+	it should "return None for missing key" in {
+		val t = new TriesST[Int]()
+		t("no-such-key") should be(None)
+	}
+	
+	it should "return None for missing key 'b'" in {
+		val t = new TriesST[Int]()
+		t("a") = 1
 		t("b") should be(None)
 	}
 	
@@ -26,6 +56,39 @@ class TriesTest extends FlatSpec with Matchers {
 		
 	}
 	
+	it should "remove a key correctly" in {
+		val t = new TriesST[String]()
+		t("abc") = "ABC"
+		t("a") = "A"
+		t("abcd") = "ABCD"
+		t("h") = "H"
+		
+		t("abcd") should be(Some("ABCD"))
+		t.size should be(4)
+		t.remove("abcd")
+		t.size should be(3)
+		t("abcd") should be(None)
+		
+	}
+	
+	"size" should "be 4 " in {
+		val t = new TriesST[String]()
+		t("abc") = "abc"
+		t("a") = "a"
+		t("abcd") = "abcd"
+		t("h") = "h"
+		t.size should be(4)
+	}
+	
+	"keys" should "reflect all" in {
+		val t = new TriesST[String]()
+		t("abc") = "abc"
+		t("a") = "a"
+		t("abcd") = "abcd"
+		t("h") = "h"
+		t.keys().toSet should be(Set("a", "abcd", "h", "abc"))
+	}
+	
 	"Option(null)" should "be None" in {
 		Option(null) should be(None)
 	}
@@ -41,4 +104,13 @@ class TriesTest extends FlatSpec with Matchers {
 		m should be(false)
 		
 	}
+	
+	it should "count number of words in sample text" in {
+		var trie = sample()
+		for (key <- trie.keys())
+			println(s"$key: ${trie(key).get}")
+		
+		true should be(true)
+	}
+	
 }
